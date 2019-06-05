874e1a4e70aece69dde7ecfcd9f87ef93d153c7c
孟鹏程 <@LAPTOP-2B2BH5BP>
2019-06-05 18:33:03 +0800

    132

--- src/day05/Test04.java
+++ src/day05/Test04.java
@@ -1,4 +1,16 @@
 package day05;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+
+
 /**
  *创建一个Map，保存某个学生的成绩:
  *在控制台输入该学生成绩，格式:
@@ -14,5 +26,55 @@ package day05;
  *
  */
 public class Test04 {
-
+	@Test
+	public void test() {
+		Map<String,Integer> map=new HashMap<String,Integer>();
+		Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
+		map.put("语文",num);
+		int num1=scan.nextInt();
+		map.put("数学", num1);
+		int num2=scan.nextInt();
+		map.put("英语", num2);
+		int num3=scan.nextInt();
+		map.put("物理",num3);
+		int num4=scan.nextInt();
+		map.put("化学", num4);
+		System.out.println(map);
+		
+		Integer str=map.get("物理");
+		System.out.println(str);
+		map.put("化学", 96);
+		
+		map.remove("英语");
+		System.out.println(map);
+		
+		System.out.println("---------------------------");
+		//用key迭代
+		Set<String> set=map.keySet();
+		
+		for(String str1:set) {
+			System.out.println(str1+"="+map.get(str1));
+		}
+		System.out.println("----------------------------");
+		//用value迭代
+		Collection<Integer> col=map.values();
+		for(Integer in:col) {
+			System.out.println(in);
+		}
+		
+		
+		System.out.println("-----------------------------");
+		//用Entry迭代
+		Set<Entry<String,Integer>> set1=map.entrySet();
+		for(Entry<String,Integer> aa:set1) {
+			System.out.println(aa.getKey()+"="+aa.getValue());
+		}
+		
+		
+		
+	}
+	
+	
+	
 }
--- src/day05/Test05.java
+++ src/day05/Test05.java
@@ -1,4 +1,13 @@
 package day05;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.junit.Test;
+
 /**
  * 有下列字符串:
  * 销售:张三;财务:李四;销售:王五;财务:赵六;程序:mike;程序:jerry;美工:jackson;前端:green;前端:nick;程序:钱七;销售:alice
@@ -9,5 +18,30 @@ package day05;
  *
  */
 public class Test05 {
-	 
+	
+	//销售：张三，王五，alice
+	//财务：李四，赵六
+	//程序：mike，jerry，钱七
+	//美工：jackson
+	//前端：green，nick
+	 @Test
+	 public void test() {
+		 Map<String,Integer> map=new HashMap<String,Integer>();
+		 map.put("销售", 3);
+		 map.put("财务", 2);
+		 map.put("程序", 3);
+		 map.put("美工", 1);
+		 map.put("前端", 2);
+		 System.out.println(map);
+		 
+		 Collection<String>col=map.keySet();
+		 for(String str:col) {
+			 System.out.println(str);
+		 }
+		 
+		 Set<Entry<String,Integer>>set=map.entrySet();
+		 for(Entry<String, Integer> en:set) {
+			 System.out.println(set);
+		 }
+	 }
 }
